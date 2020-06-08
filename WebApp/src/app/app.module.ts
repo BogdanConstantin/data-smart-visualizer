982d3e5802c11b2a1bd6b0c500763777716e97ec
BogdanConstantin <37022813+BogdanConstantin@users.noreply.github.com>
2020-06-09 02:33:25 +0300

    Created angular project

---
+++ WebApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxInputSearchModule } from 'ngx-input-search';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { DatasetsListComponent } from './datasets-list/datasets-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DatasetsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxInputSearchModule,
+    NgSelectModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WebApp/src/app/datasets-list/datasets-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datasets-list',
+  templateUrl: './datasets-list.component.html',
+  styleUrls: ['./datasets-list.component.scss']
+})
+export class DatasetsListComponent implements OnInit {
+
+  defaultBindingsList = [
+    { value: 1, label: 'Newest' },
+    { value: 2, label: 'Most liked' },
+    { value: 3, label: 'Most disliked'}
+  ];
+
+  selectedOption = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
